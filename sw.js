ddb18c6158f71f93a26c1e342df895c00538be73
meetellieai <jaxon.pml@gmail.com>
2026-03-19 20:29:48 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'archie-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/music.mp3'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
